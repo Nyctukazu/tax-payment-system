2cd39f5ed9ab9ab7af9fa944d4627236c12ef39c
Nyctukazu <laurenceryancruz@gmail.com>
2026-06-10 10:46:21 +0800

    feat(frontend): add smooth hover elevation and universal click ripple to buttons

--- src/main/java/gov/pasay/taxsystem/controller/PortalController.java
+++ src/main/java/gov/pasay/taxsystem/controller/PortalController.java
@@ -8,7 +8,7 @@ public class PortalController {
     
     @GetMapping("/portal")
     public String showPortalLandingPage() {
-        return "index";
+        return "admin/admin-login";
     }
 
 }
