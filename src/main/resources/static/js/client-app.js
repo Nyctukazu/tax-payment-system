ba1bfcc3f5c94ca6fe754c8053191e13e29f742a
Nyctukazu <laurenceryancruz@gmail.com>
2026-06-11 22:49:14 +0800

    fix(frontend): organize and clean the current frontend code

--- src/main/resources/static/js/client-app.js
+++ src/main/resources/static/js/client-app.js
@@ -1,9 +1,16 @@
 import { initHeader } from './components/AppHeader.js';
 import { initSidebar } from './components/AppSidebar.js';
+import { initializeParticles } from "./components/particles.js";
+import { passwordValidator } from "./services/password-validation.js";
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Global Backbone Initialized.");
-    
+    passwordValidator();
+    initializeParticles();
     initHeader();
     initSidebar();
 });
--- src/main/resources/static/js/components/particles.js
+++ src/main/resources/static/js/components/particles.js
@@ -1,9 +1,10 @@
 
-
 export function initializeParticles() {
     const canvas = document.getElementById("particle-canvas");
-    if (!canvas) return;
+    const canvas2 = document.getElementById("particle-canvas2");
+    if (!canvas && !canvas2) return;
 
+    const activeCanvas = canvas || canvas2;
     const isMobile = window.innerWidth < 768;
 
     const particleCount = isMobile ? 25 : 50;
@@ -17,11 +18,14 @@ export function initializeParticles() {
 
         const startTop = 105; 
         
-        const randomScale = Math.random() * 1.2 + 0.4; 
+        const randomScale = Math.random() * 2 + 0.4; 
+        const randomOpacity = randomScale/4.8;
         const randomRotation = Math.random() * 360; 
 
         const randomDuration = Math.random() * 15 + 15; 
         const randomDelay = Math.random() * -20; 
+
+        const color = canvas ? '20px solid #580B74' : '20px solid #F98513';
 
         triangle.style.left = `${randomLeft}%`;
         triangle.style.top = `${startTop}vh`;
@@ -29,7 +33,9 @@ export function initializeParticles() {
         triangle.style.scale = randomScale;
         triangle.style.animationDuration = `${randomDuration}s`;
         triangle.style.animationDelay = `${randomDelay}s`;
+        triangle.style.opacity = randomOpacity;
+        triangle.style.borderBottom = color;
 
-        canvas.appendChild(triangle);
+        activeCanvas.appendChild(triangle);
     }
 }
