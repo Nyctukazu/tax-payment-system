dc1ce1382aa304c5a4dfdd062c02a93229afe74e
Nyctukazu <laurenceryancruz@gmail.com>
2026-06-12 15:24:06 +0800

    feat(frontend): fix the position of the eye button

--- src/main/resources/static/js/start-app.js
+++ src/main/resources/static/js/start-app.js
@@ -8,3 +8,23 @@ document.addEventListener("DOMContentLoaded", () => {
     initStartHeader();
 
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginCard = document.getElementById("login-card");
+    const forgotCard = document.getElementById("forgot-card");
+    const toForgotBtn = document.getElementById("to-forgot-btn");
+    const toLoginBtn = document.getElementById("to-login-btn");
+
+    if (toForgotBtn && toLoginBtn) {
+        toForgotBtn.addEventListener("click", () => {
+            loginCard.classList.add("hidden");
+            forgotCard.classList.remove("hidden");
+        });
+
+        toLoginBtn.addEventListener("click", () => {
+            forgotCard.classList.add("hidden");
+            loginCard.classList.remove("hidden");
+        });
+    }
+});
+
